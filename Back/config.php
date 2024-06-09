397bad20168947514e6660a664b72fa96e2ccad5
lucasdomingueslvr <lucasdomingues@unipam.edu.br>
2024-06-09 18:30:29 -0300

    lucas conseguiu no novo bd

--- Back/config.php
+++ Back/config.php
@@ -2,8 +2,8 @@
 
     $dbHost = 'localhost';
     $dbUsername = 'root';
-    $dbPassword = '123mudar';
-    $dbName = 'mellrystore';
+    $dbPassword = '123mudar'; //lucas conseguiu
+    $dbName = 'bdmelrystore';
 
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 ?>
